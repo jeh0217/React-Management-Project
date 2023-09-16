89b8dbc28ddb22ec776f0d47762626c0bd461409
enho <dmsgh9219@naver.com>
2023-09-16 18:36:20 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 const customers = [
   {
     'id': 1,
-    'image': 'http://via.placeholder.com/60x60/BDBDBD/000000?text=60X60', //해당 링크는 랜덤 이미지 웹사이트
-    'name': 'enho',
+    'image': 'http://via.placeholder.com/60x60/BDBDBD/000000?text=64X64', //해당 링크는 랜덤 이미지 웹사이트
+    'name': '은호',
     'birthday': '000217',
     'gender': '남자',
     'job': '대학생'
@@ -14,15 +14,15 @@ const customers = [
   {
     'id': 2,
     'image': 'http://via.placeholder.com/60x60/BDBDBD/000000?text=60X60', //해당 링크는 랜덤 이미지 웹사이트
-    'name': '효효',
+    'name': '효주',
     'birthday': '990505',
-    'gender': '남자',
-    'job': '셀비오'
+    'gender': '여자',
+    'job': '관리사'
   },
   {
     'id': 3,
     'image': 'http://via.placeholder.com/60x60/BDBDBD/000000?text=60X60', //해당 링크는 랜덤 이미지 웹사이트
-    'name': '호호',
+    'name': '호래',
     'birthday': '000112',
     'gender': '남자',
     'job': '디자인'
@@ -34,26 +34,8 @@ class App extends Component {
   render() {
     return (
       <div>  
-        {   
-        //map함수를 이용 긴 코드가 짧아짐으로써 수월해짐
-        customers.map(c => {
-          return (
-          <Customer
-            key={c.id} //key 이용 안하면 자바스크립트 콘솔에 오류 뜸
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-            
-            />
-            );
-        
-        })
-      }
+        {customers.map(c => { return (<Customer key={c.id}id={c.id}image={c.image}name={c.name}birthday={c.birthday}gender={c.gender}job={c.job}/>);})}
       </div>
-
     );
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-class Customer extends React.Component {
 
+class Customer extends React.Component {
     render() {
         return (
             <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+              <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+              <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
             </div>
+
         )
     }
 }
-
 
 class CustomerProfile extends React.Component {
     render() {
         return (
             <div>
                 <img src={this.props.image} alt="profile" />
-                <h2>{this.props.name}({this.props.id})</h2>
+                <h2>{this.props.name} {this.props.id}</h2>
             </div>
         )
     }
@@ -35,6 +35,8 @@ class CustomerInfo extends React.Component {
         )
     }
 }
+
+
 
 
 export default Customer;
